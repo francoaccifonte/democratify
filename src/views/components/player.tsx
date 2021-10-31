650c5ce44cfd69d4559fe3ec88bca77a0d8d63b0
francoaccifonte <franco.accifonte@gmail.com>
2021-10-31 20:41:21 -0300

    add spotify login button (#2)
    * add spotify login button
    
    * remove second login button

--- src/views/components/player.tsx
+++ src/views/components/player.tsx
@@ -7,12 +7,22 @@ import Row from 'react-bootstrap/Row'
 import { useSelector, useDispatch } from 'react-redux'
 import { incrementPoolSize, decrementPoolSize } from '../../features/slices/current_playlist_slice'
 import { RootState } from '../../features/root_reducer'
+import SpotifyLoginButton from './spotify_login_button'
 
 const Player = () => {
   const image = Faker.image.imageUrl;
   const candidatePoolSize = useSelector((state: RootState) => state.currentPlaylist.candidatePoolSize)
   const dispatch = useDispatch();
-  
+
+  const state = '34fFs29kd09';
+  const scopes = ['user-read-email', 'playlist-read-private', 'playlist-read-collaborative', 'user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing']
+  const loginURI = "https://accounts.spotify.com/authorize?" +
+  "response_type=code&" +
+  "client_id=9d48abfbbf194adc9051e1b82b0ecdb0&" +
+  `scope=${scopes.join("%20")}&` +
+  "redirect_uri=http://localhost:3001/spotify_login&" +
+  `state=${state}`
+
   return(
     <div className="player">
       <Image src={image()} alt={"asdfg"} rounded fluid/>
@@ -29,6 +39,8 @@ const Player = () => {
           <Button variant="info" onClick={() => dispatch(incrementPoolSize())}>+</Button>
         </ButtonGroup>
       </Container>
+
+      <SpotifyLoginButton />
     </div>
     )
 }
